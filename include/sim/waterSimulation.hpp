c4f75850ff5a75fc5bc6917492f319be5f182d53
Awab <awab.qureshi@gmail.com>
2025-07-16 11:51:11 -0400

    Increase numerical stability
    Prevent situation where simulation would become unstable with small
    velocity waves (towards the end of the simulation in this case).
    
    Decrease timestep to prevent instability and decrease sleep time to
    compensate. (make sure you are not on battery saver!!)

--- include/sim/waterSimulation.hpp
+++ include/sim/waterSimulation.hpp
@@ -34,19 +34,26 @@ class WaterSimulation
     // use a fractional value of the bound (0.2 is suggested by Fluid
     // Simulation for Computer Graphics by R. Bridson in section 12.3).
     // We use the current value discovered via trial and error.
-    constexpr static float deltaT = deltaTBoundAbove * 0.2F;
+    constexpr static float deltaT = deltaTBoundAbove * 0.1F;
     // Other bounds on deltaT:
     // - t_n + deltaT < t_frame  [SCG by R.B. section 2.3]
     // -
 
     // TODO: HOW TO BOUND THE WAVE SIZE AAAAA
-    // TODO: come up with an actual min speed value if we even want this
+    // TODO: come up with an actual min speed value if we even want this...it
+    // would break conservation of energy?
     constexpr static float minSpeedComponent = 0.0F;
 
     constexpr static float maxSpeedComponent = ([]() {
         constexpr float alpha = 0.5F;
         return alpha * cellSize / deltaT;
     })();
+
+    // Some tiny loss of energy for velocity components
+    // TODO: Come up with an actual value for this guy
+    constexpr static float velocityComponentDissipationConstant = 0.99985F;
+
+    // TODO: Check for volume conservation every N timesteps?
 
   private:
     StaggeredVelocityGrid<numRows, numCols> velocityGrid{minSpeedComponent,
